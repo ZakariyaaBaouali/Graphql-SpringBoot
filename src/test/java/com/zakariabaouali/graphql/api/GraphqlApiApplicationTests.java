6b41fe4191b4499dce371a9a5f292d82530b89ec
zakaria <zakariabaouali@outlook.com>
2024-08-08 14:53:04 +0100

    init commit

---
+++ src/test/java/com/zakariabaouali/graphql/api/GraphqlApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zakariabaouali.graphql.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphqlApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
